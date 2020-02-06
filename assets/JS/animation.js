3d88b6626f4748f34d0908db619b1c147eabdd29
dilanSuraj <it17121002@my.sliit.lk>
2020-02-06 21:11:31 +0530

    page l,oader added

--- assets/JS/animation.js
+++ assets/JS/animation.js
@@ -1,9 +1,15 @@
 function play(event) {
-    var href =  event.target.parentNode.href;
+    var href = event.target.parentNode.href;
     event.preventDefault();
     var audio = document.getElementById("audio");
     audio.play();
+    
     audio.addEventListener('ended', function () {
         window.location = href;
     });
 }
+
+$(window).on("load",function(){
+        // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
